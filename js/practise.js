830a95e2870904d667d6b447c62536c2d97d5aad
Sehrish Talat <133611271+SehrishTalat@users.noreply.github.com>
2024-11-18 20:52:44 +0500

    Add files via upload

---
+++ js/practise.js
@@ -0,0 +1,12 @@
+//show something on browser
+//Document Commands 
+document.write("Hello World");
+
+//Add HTML Tag in JS
+document.write("Hello World<br>");
+document.write("<i>Hello World</i>");
+document.write("<b>Hello World</b>");
+
+
+//allert box
+alert("Hello WORLD");
