70a5937349a51adc4dc71cdfc47c50401ca3a784
Sehrish Talat <133611271+SehrishTalat@users.noreply.github.com>
2024-11-20 07:23:45 +0500

    Update practise.js

--- js/practise.js
+++ js/practise.js
@@ -10,3 +10,96 @@ document.write("<b>Hello World</b>");
 
 //allert box
 alert("Hello WORLD");
+
+
+
+
+
+/*----------------Tuesday, November 19, 2024--------------*/
+
+
+//JavaScript Display Possibilities
+//Using innerHTML
+document.getElementById('para5').innerHTML= 11;
+
+//Using document.write()
+document.write(11);
+
+//window alert
+window.alert(11);
+
+
+//js statements
+let a, b, c;  // Declare 3 variables
+a = 5;        // Assign the value 5 to a
+b = 6;        // Assign the value 6 to b
+c = a + b;    // Assign the sum of a and b to c
+
+//multiple statements on one line are allowed, separated by semicolons
+a = 5; b = 6; c = a + b; 
+
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+let z = x + y;
+
+
+//JavaScript Literals - fixed value
+document.getElementById('para_literal').innerHTML = 10;
+document.getElementById('para_literal').innerHTML = 'Sehrish';
+
+
+
+//keyword use - var
+var a, y;
+x = 5 + 6;
+y = x * 10;
+document.getElementById("demo").innerHTML = y;
+
+
+//declaring and assign value to a variable
+let carName = "Volvo";
+document.getElementById("para").innerHTML = carName;
+
+//declaring and assigning value
+let x;
+x = 6;
+document.getElementById("demo").innerHTML = x;
+
+
+//declaring, intializing and use of arithmetic operators
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML = "The value of z is: " + z;
+
+
+//arithemtic operator use
+let x, y;
+x = 5;
+y = 6;
+document.getElementById("demo").innerHTML = x + y;
+
+
+//variable overwrite with newest value
+var X = "Hello World";				
+X = "new value"					
+
+document.write(X);
+
+//expressions
+document.getElementById("demo").innerHTML = 5 * 10;
+
+//Expressions can also contain variable values:   x * 10
+document.getElementById("demo").innerHTML = x * 10;
+
+document.getElementById("demo").innerHTML = "John" + " "  + "Doe";
+
+
+
+
+/*----------------Wednesday, November 20, 2024--------------*/
